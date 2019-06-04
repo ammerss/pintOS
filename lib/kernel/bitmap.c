2022c63254e833c99ed971f6e3501f67df4498a6
weenybeenymini <alsrjs2516@naver.com>
2019-06-04 20:31:26 +0900

    add_buddy_system

--- lib/kernel/bitmap.c
+++ lib/kernel/bitmap.c
@@ -4,6 +4,7 @@
 #include <round.h>
 #include <stdio.h>
 #include "threads/malloc.h"
+#include "threads/palloc.h" //option에 따라 다른 할당 방식을 사용하기 위해 추가
 #ifdef FILESYS
 #include "filesys/file.h"
 #endif
@@ -305,83 +306,214 @@ bitmap_scan (const struct bitmap *b, size_t start, size_t cnt, bool value)
   ASSERT (b != NULL);
   ASSERT (start <= b->bit_cnt);
 
+  static size_t latest = 0; // 직전에 할당한 위치에 대한 정보를 담고 있는 변수이다.
+  static size_t check_cnt = 0; // 실행 작업을 위해 해당 함수가 호출되는 횟수를 담고 있는 변수이다.
+  static size_t check_size = 0; // 실행 작업을 위해 할당되는 공간의 크기를 담고 있는 변수이다.
+
   if (cnt <= b->bit_cnt) 
     {
       size_t last = b->bit_cnt - cnt;
       size_t i;
-      for (i = start; i <= last; i++)
-        if (!bitmap_contains (b, i, cnt, !value))
-          return i; 
-    }
-  return BITMAP_ERROR;
-}
-
-size_t
-bitmap_scan_extend (const struct bitmap *b, size_t start, size_t end, size_t cnt, bool value) 
-{
-  ASSERT (b != NULL);
-  ASSERT (start <= b->bit_cnt);
-
-  if (cnt <= b->bit_cnt) 
-    {
-      size_t i;
-      for (i = start; i <= end - cnt; i++)
-        if (!bitmap_contains (b, i, cnt, !value))
-          return i; 
-    }
-  return BITMAP_ERROR;
-}
-
-size_t
-bitmap_scan_min (const struct bitmap *b, size_t start, size_t cnt, bool value) 
-{
-  size_t min_idx = bitmap_scan (b, start, cnt, value);
-  if(min_idx == BITMAP_ERROR){
-      return min_idx;
-  }else{
-      size_t last = b->bit_cnt - cnt;
-      size_t i;
-      size_t fragment;
-      for(i = 1; i <= last - (min_idx + cnt) ; i++){
-	if (bitmap_test (b, min_idx + cnt + i) != false){
-	  i--;
-          break;
+      if(option == 0){ //원래 pintos에서 구현되어 있던 first fit
+	for (i = start; i <= last; i++){
+          if (!bitmap_contains (b, i, cnt, !value))
+            return i;
 	}
-      }
-      fragment = i;
 
-      for (i = min_idx + cnt + fragment + 1; i <= last; i++){
-        if (!bitmap_contains (b, i, cnt, !value)){
-	  size_t j;
-	  for(j = 1; j <= last - (i + cnt) ; j++){
-	    if (bitmap_test (b, i + cnt + j) != false){
-		j--;
-          	break;
+      }else if(option == 1){ //next fit
+	// 제일 최근에 할당한 위치에서부터 검색
+        for (i = latest; i <= last; i++){
+          if (!bitmap_contains (b, i, cnt, !value))
+          {
+            latest = i; //제일 최근 할당 위치 갱신
+            return i;
+          }
+	}
+	// 앞선 반복문에서 할당할 수 있는 위치를 찾지 못한 경우 메모리의 시작 지점부터 직전에 할당한 위치까지 검색
+        for (i = start; i <= latest; i++){
+          if (!bitmap_contains (b, i, cnt, !value))
+          {
+            latest = i; //제일 최근 할당 위치 갱신
+            return i;
+          }
+	}
+
+      }else if(option == 2){ //best fit
+	size_t min_idx = BITMAP_ERROR; //내부 단편화가 가장 작은 페이지 위치를 저장하는 변수
+	for (i = start; i <= last; i++){
+          if (!bitmap_contains (b, i, cnt, !value))
+            return min_idx = i;
+	} //first fit으로 맨 처음 위치로 부터 할당할 수 있는 페이지 위치를 저장 
+	if(min_idx == BITMAP_ERROR){
+      	  return min_idx; // 할당할 수 있는 페이지 자체가 없는 경우
+	}else{
+	  size_t fragment; // 사용 가능 하지만 사용하지 않는 남는 공간의 크기를 저장하는 변수
+	  //fragment 가 가장 적은 페이지 위치를 반환하는 것이 best fit
+	  for(i = 1; i <= last - (min_idx + cnt) ; i++){ //i의 크기를 증가시켜가며, 남은 공간을 측정
+	    if (bitmap_test (b, min_idx + cnt + i) != false){
+	      i--;
+              break;
 	    }
 	  }
-	  if(fragment > j){	
+	  fragment = i;
+
+	  for (i = min_idx + cnt + fragment + 1; i <= last; i++){
+	    //첫 번째 찾은 페이지 이후로
+	    if (!bitmap_contains (b, i, cnt, !value)){
+	      // 할당 가능한 페이지를 찾으면
+	      for(size_t j = 1; j <= last - (i + cnt) ; j++){
+		if (bitmap_test (b, i + cnt + j) != false){
+		  j--;
+          	  break;
+	        }
+	      }
+	      // 그 페이지의 남은 공간을 측정하고
+	      if(fragment > j){	
+	      //fragment보다 작으면 min_idx를 갱신한다
 		min_idx = i;
 		fragment = j;
+	      }
+	    }
 	  }
+	  return min_idx; //끝까지 검색하고 min_idx 반환 
 	}
+      }else if(option == 3){ //buddy system
+	
+	size_t max = 512; // Buddy System 기법을 사용하였을 때, 할당할 수 있는 최대 공간 크기이다.
+        size_t bound = 512; // 요청 크기에 가장 가까운 빈 공간의 크기를 찾는데 도움을 주는 변수이다.
+        size_t offset = 0; // 할당되어 있는 공간의 2^k 크기를 구하는 변수이다.
+        size_t j = 0; // 해당 위치가 빈 공간인지 아닌지 알고자 반복문에서 사용하는 변수이다.
+
+        if(check_cnt < 3)
+        {
+          i = 0; // 요청 크기에 가장 가까운 빈 공간의 위치를 담고 있는 변수이다.
+          check_cnt++;
+        }
+        else
+        {
+          i = check_size;
+        }
+
+        if(cnt > max)
+        { // 요청 크기가 할당할 수 있는 최대 크기 공간보다 클 경우에 대한 예외 처리이다.
+          return BITMAP_ERROR;
+        }
+
+        while(true)
+        { // 요청 크기에 가장 가까운 빈 공간을 찾는 반복문이다.
+          bound = bound / 2;
+
+          if(cnt > bound)
+          {
+            while(true)
+            {
+              if(i >= max + check_size)
+              { // 요청 크기에 가장 가까운 빈 공간을 찾을 수 없을 경우에 대한 예외 처리이다.
+                return BITMAP_ERROR;
+              }
+
+              if(bitmap_test (b, i) == true)
+              { // 해당 위치가 할당되어 있을 경우, 할당되어 있는 공간의 2^k 크기를 구한다.
+                j = i;
+                while(true)
+                { // 할당되어 있는 공간의 크기를 구하는 반복문이다.
+                  if(j >= max + check_size)
+                  { // 빈 공간이 없는 경우에 대한 예외 처리이다.
+                    return BITMAP_ERROR;
+                  }
+
+                  if(bitmap_test (b, j) == true)
+                  {
+                    offset++;
+                  }
+                  else
+                  {
+                    break;
+                  }
+
+                  j++;
+                }
+
+                if(offset > 256)
+                { // 빈 공간이 없는 경우에 대한 예외 처리이다.
+                  offset = 512;
+                  return BITMAP_ERROR;
+                }
+                else if(offset > 128)
+                { // 해당 if else 문들은 offset 변수의 값을 보다 큰 2^k 로 바꾼다.
+                  offset = 256;
+                }
+                else if(offset > 64)
+                {
+                  offset = 128;
+                }
+                else if(offset > 32)
+                {
+                  offset = 64;
+                }
+                else if(offset > 16)
+                {
+                  offset = 32;
+                }
+                else if(offset > 8)
+                {
+                  offset = 16;
+                }
+                else if(offset > 4)
+                {
+                  offset = 8;
+                }
+                else if(offset > 2)
+                {
+                  offset = 4;
+                }
+                else if(offset > 1)
+                {
+                  offset = 2;
+                }
+                else
+                {
+                  offset = 1;
+                }
+
+                if(offset > bound * 2)
+                {
+                  i = i + offset;
+                }
+                else
+                {
+                  i = i + bound * 2;
+                }
+              }
+              else
+              {
+                if(bound != 0)
+                {
+                  if(!bitmap_contains (b, i, bound * 2, !value))
+                  {
+                    printf("할당된 위치 i: %10d,    ", i-3);
+                    printf("요청 크기 cnt: %10d\n", cnt);
+                    return i;
+                  }
+
+                  i = i + bound * 2;
+                }
+                else
+                {
+                  if(bitmap_test (b, i) == false)
+                  {
+                    printf("할당된 위치 i: %10d,    ", i-3);
+                    printf("요청 크기 cnt: %10d\n", cnt);
+                    return i;
+                  }
+
+                  i = i + 1;
+                }
+              }
+            }
+          }
+        }
       }
-      return min_idx;
-  }
-}
-
-size_t
-bitmap_scan_buddy (const struct bitmap *b, size_t start, size_t cnt, bool value) 
-{
-  ASSERT (b != NULL);
-  ASSERT (start <= b->bit_cnt);
-
-  if (cnt <= b->bit_cnt) 
-    {
-      size_t last = b->bit_cnt - cnt;
-      size_t i;
-      for (i = start; i <= last; i++)
-        if (!bitmap_contains (b, i, cnt, !value))
-          return i; 
     }
   return BITMAP_ERROR;
 }
@@ -397,42 +529,6 @@ size_t
 bitmap_scan_and_flip (struct bitmap *b, size_t start, size_t cnt, bool value)
 {
   size_t idx = bitmap_scan (b, start, cnt, value);
-  if (idx != BITMAP_ERROR) 
-    bitmap_set_multiple (b, idx, cnt, !value);
-  return idx;
-}
-
-
-size_t
-bitmap_scan_extend_and_flip (struct bitmap *b, size_t start, size_t end, size_t cnt, bool value)
-{
-  size_t idx = bitmap_scan_extend (b, start, end, cnt, value);
-  if (idx != BITMAP_ERROR) 
-    bitmap_set_multiple (b, idx, cnt, !value);
-  return idx;
-}
-
-size_t
-bitmap_scan_min_and_flip (struct bitmap *b, size_t start, size_t cnt, bool value)
-{
-  size_t idx = bitmap_scan_min (b, start, cnt, value);
-  if (idx != BITMAP_ERROR) 
-    bitmap_set_multiple (b, idx, cnt, !value);
-  return idx;
-}
-
-size_t
-bitmap_scan_buddy_and_flip (struct bitmap *b, size_t start, size_t cnt, bool value)
-{
-  //expand cnt to square
-  int square = 1;
-  while(cnt > square){
-    square *= 2;
-  }
-  cnt = square;
-
-  size_t idx = bitmap_scan_buddy (b, start, cnt, value);
-
   if (idx != BITMAP_ERROR) 
     bitmap_set_multiple (b, idx, cnt, !value);
   return idx;
