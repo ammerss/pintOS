ae6823eaf53c68805daf9e105ddc339d55f09801
celi1004 <celi1004@cau.ac.kr>
2019-06-05 02:41:55 -0700

    frame.c

--- vm/frame.c
+++ vm/frame.c
@@ -6,7 +6,8 @@
 #include "userprog/pagedir.h"
 #include "threads/palloc.h"
 #include "threads/malloc.h"
-
+#include "vm/swap.h"
+#include "vm/page.h"
 #include "vm/frame.h"
 
 
@@ -203,6 +204,7 @@ void vm_free_frame(void *frame) {
 void vm_set_frame(void *frame, void *pte, void *upage){
 	struct frame *f, *find;
 	struct list_elem *e;
+	f=frame;
 	e=list_head(&frame_list);
 	while((e=list_next(e)) != list_tail (&frame_list)){
 		find=list_entry(e, struct frame, elem);
