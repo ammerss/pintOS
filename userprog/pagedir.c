d4891ece026867f0acbf1b641ef1debd60d0491c
celi1004 <33647623+celi1004@users.noreply.github.com>
2019-06-05 01:01:29 -0700

    add lru_list

--- userprog/pagedir.c
+++ userprog/pagedir.c
@@ -9,6 +9,8 @@
 static uint32_t *active_pd (void);
 static void invalidate_pagedir (uint32_t *);
 
+static struct list lru_list;
+
 /* Creates a new page directory that has mappings for kernel
    virtual addresses, but none for user virtual addresses.
    Returns the new page directory, or a null pointer if memory
@@ -204,14 +206,38 @@ pagedir_set_accessed (uint32_t *pd, const void *vpage, bool accessed)
   uint32_t *pte = lookup_page (pd, vpage, false);
   if (pte != NULL) 
     {
-      if (accessed)
+      if (accessed){
         *pte |= PTE_A;
+         pagedir_lru_list_add(vpage);
+      }
       else 
         {
           *pte &= ~(uint32_t) PTE_A; 
           invalidate_pagedir (pd);
         }
     }
+}
+
+void
+pagedir_lru_list_add(const void *vpage){
+   
+   for(list_elem* e=list_head(&lru_list); e != list_tail(&list_lru); e=list_next(e)){
+      if(e == vpage){
+         list_remove(e);
+         list_push_back(&lru_list, e);
+         break;
+         return;
+      }
+   }
+   
+   list_push_back(&lru_list, vpage);
+   return;
+}
+
+void*
+pagedir_lru_list_get_head(){
+   
+   return list_head(&lru_list);
 }
 
 /* Loads page directory PD into the CPU's page directory base
